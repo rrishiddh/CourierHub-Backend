e68ec42ea56614fcb3e54d36bb54c44bbb8ad902
rrishiddh <rrishiddh@gmail.com>
2025-08-27 19:02:04 +0600

    fix: cors issue updated

--- src/app.ts
+++ src/app.ts
@@ -13,11 +13,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 connectDB();
-const allowedOrigins = process.env.CORS_ORIGINS ? process.env.CORS_ORIGINS.split(',') : [];
 
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
     credentials: true,
   })
 );
